4dcd5b4a031dda6f9bd9a6485dbf0bab2f352d0f
farmme-dev-chunglt <chunglt@farmme.io>
2022-12-22 15:18:17 +0700

    login

--- resources/js/components/login/login.vue
+++ resources/js/components/login/login.vue
@@ -1,10 +1,70 @@
 <template>
-  <h1>123</h1>
+   <div class="login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="#"> <b>Admin</b>LTE </a>
+      </div>
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign in to start your session</p>
+          <Form  @submit="submit(dataProps.slug)"
+                      :validation-schema="schema"
+                      v-slot="{ errors }"
+                      ref="form">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Name</label>
+                        <Field
+                          type="text"
+                          name="account"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          placeholder="Enter email"
+                          aria-describedby="exampleInputEmail1-error"
+                          aria-invalid="true"
+                          :rules="{ required: true,min: 6 }"
+                        />
+                        <span id="exampleInputEmail1-error" class="error">
+                          {{ errors.account }}
+                        </span>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Name</label>
+                        <Field
+                          type="password"
+                          name="password"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          placeholder="Enter email"
+                          aria-describedby="exampleInputEmail1-error"
+                          aria-invalid="true"
+                          :rules="{ required: true,min: 6 }"
+                        />
+                        <span id="exampleInputEmail1-error" class="error">
+                          {{ errors.password }}
+                        </span>
+                      </div>
+            <div class="row">
+              <div class="col-8">
+                <div class="icheck-primary">
+                  <input type="checkbox" id="remember" />
+                  <label for="remember"> &nbsp; Remember Me </label>
+                </div>
+              </div>
+              <div class="col-4">
+                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+              </div>
+            </div>
+          </Form>
+          <p class="mb-0">
+            <a href="register.html" class="text-center">Register a new membership</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  created() {
-    this.$router.push('product')
-  }
+  
 }
 </script>
