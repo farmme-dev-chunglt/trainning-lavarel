cbf11ed0e5a049e2323e4e893a5259dba309495e
farmme-dev-chunglt <chunglt@farmme.io>
2022-12-22 15:05:14 +0700

    login

--- resources/js/components/login/login.vue
+++ resources/js/components/login/login.vue
@@ -1,10 +1,49 @@
 <template>
-  <h1>123</h1>
+  <div class="login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="../../index2.html"> <b>Admin</b>LTE </a>
+      </div>
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign in to start your session</p>
+          <form >
+            <div class="input-group mb-3">
+              <input type="email" class="form-control" placeholder="Email" />
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="password" class="form-control" placeholder="Password" />
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-8">
+                <div class="icheck-primary">
+                  <input type="checkbox" id="remember" />
+                  <label for="remember"> &nbsp; Remember Me </label>
+                </div>
+              </div>
+              <div class="col-4">
+                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+              </div>
+            </div>
+          </form>
+          <p class="mb-0">
+            <a href="register.html" class="text-center">Register a new membership</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-  created() {
-    this.$router.push('product')
-  }
-}
+export default {}
 </script>
